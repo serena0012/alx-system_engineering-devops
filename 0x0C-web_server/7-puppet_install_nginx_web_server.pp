c851660f347fc3a531a88850465f597208be2d8a
Gbenga Elegbede <jonathangnenga@gmail.com>
2023-03-29 00:57:24 +0100

    README.md updated

--- 0x0C-web_server/7-puppet_install_nginx_web_server.pp
+++ 0x0C-web_server/7-puppet_install_nginx_web_server.pp
@@ -1,4 +1,4 @@
-cript to install nginx using puppet
+# Script to install nginx using puppet
 
 package {'nginx':
   ensure => 'present',
@@ -15,7 +15,7 @@ exec {'Hello':
   provider => shell,
 }
 
-exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/blog.ehoneahobed.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
+exec {'sudo sed -i "s/listen 80 default_server;/listen 80 default_server;\\n\\tlocation \/redirect_me {\\n\\t\\treturn 301 https:\/\/google.com\/;\\n\\t}/" /etc/nginx/sites-available/default':
   provider => shell,
 }
 
